4a7b1f863655901c46549d490cd5b801f1a8c11e
ismaellawrenz <ismaellawrenz@gmail.com>
2021-11-15 20:54:31 -0300

    Criacao do README

--- src/main/java/com/discord/servicos/DiscordServico.java
+++ src/main/java/com/discord/servicos/DiscordServico.java
@@ -10,19 +10,25 @@ import org.springframework.stereotype.Component;
 @Component
 public class DiscordServico {
 
-	private final String token = "OTA3Nzc0NTIxOTIxMTIyMzY0.YYsE5w.v2SJOoN2GeJ-2UAkUsQpGHFQm5Y";
+	
+	private final LocalDate meuAniversario = LocalDate.of(LocalDate.now().getYear(), 8, 18);
 
 	public DiscordServico() {
-		System.out.println(LocalDate.of(LocalDate.now().plusYears(1).getYear(), 8, 18));
-		DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
+		
+		DiscordApi api = new DiscordApiBuilder().setToken(Chaves.token).login().join();
 		api.addMessageCreateListener(event -> {
 			if (event.getMessageContent().equalsIgnoreCase("olá")) {
 				event.getChannel().sendMessage("Olá " + event.getMessageAuthor().getName());
 			}
-			if (event.getMessageContent().equalsIgnoreCase("Quantos dias faltam para meu aniversario?")) {
+			if (event.getMessageContent().equalsIgnoreCase("Quantos dias faltam para meu aniversário?")) {
+				long diasFaltantes;				
+				if(meuAniversario.isBefore(LocalDate.now())) {
+					diasFaltantes =  ChronoUnit.DAYS.between(LocalDate.now(), meuAniversario.plusYears(1));
+				}else {
+					diasFaltantes = ChronoUnit.DAYS.between(LocalDate.now(), meuAniversario);					
+				}				
 				event.getChannel().sendMessage("Olá "
-						+ event.getMessageAuthor().getName() + " faltam exatamente " + ChronoUnit.DAYS
-								.between(LocalDate.now(), LocalDate.of(LocalDate.now().plusYears(1).getYear(), 8, 18))
+						+ event.getMessageAuthor().getName() + " faltam exatamente " + diasFaltantes
 						+ " dias.");
 			}
 
